d68b111ec761b1b5ae231f9f7f19f1925b621e4d
James_Pope <jamesxpope@gmail.com>
2024-07-12 14:28:06 -0400

    FormSetup

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 import Registration from "./components/Registration/Registration";
-import "./App.css";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(
@@ -13,18 +17,21 @@ function App() {
 
   return (
     <>
+      <div className="header">
+        <h1>User Management</h1>
+      </div>
       <Router>
         <Nav loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
         <Routes>
-          <Route path="/" element={<Home />}></Route>
           <Route
-            path="/login"
-            element={<Login setLoggedIn={setLoggedIn} />}
-          ></Route>
+            path="/"
+            element={loggedIn ? <Home /> : <Navigate to="/login" />}
+          />
+          <Route path="/login" element={<Login setLoggedIn={setLoggedIn} />} />
           <Route
             path="/registration"
             element={<Registration setLoggedIn={setLoggedIn} />}
-          ></Route>
+          />
         </Routes>
       </Router>
     </>
--- React/src/components/Registration/Registration.jsx
+++ React/src/components/Registration/Registration.jsx
@@ -1,5 +1,109 @@
-import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+// import { useRegisterMutation } from "./RegisterSlice";
 
-export default function Registration() {
-  return <div>Registration</div>;
+export default function Register(setLoggedIn) {
+  // const [registerUser] = useRegisterMutation();
+
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+  });
+
+  const updateForm = (e) => {
+    setForm((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  // const submit = async (e) => {
+  //   e.preventDefault();
+  //   console.log("This is the form:", form);
+  //   try {
+  //     let success = false;
+  //     success = await registerUser(form).unwrap();
+  //     if (success) {
+  //       window.sessionStorage.setItem("Token", success.token);
+  //       setLoggedIn(true);
+  //       navigate("/");
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  const toLoginRedirect = () => {
+    navigate("/login");
+  };
+
+  return (
+    <div className="form-container">
+      <h2 className="form-header">Register</h2>
+      {/* <form onSubmit={submit}> */}
+      <form>
+        <div className="form-group">
+          <label>First Name</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="First Name"
+            name="firstname"
+            // value={form.firstname}
+            onChange={updateForm}
+          />
+        </div>
+        <div className="form-group">
+          <label>Last Name</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Last Name"
+            name="lastname"
+            // value={form.lastname}
+            onChange={updateForm}
+          />
+        </div>
+        <div className="form-group">
+          <label>Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+            name="email"
+            // value={form.email}
+            onChange={updateForm}
+          />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Password"
+            name="password"
+            // value={form.password}
+            onChange={updateForm}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+      <div className="redirect-container">
+        <h3>Already Have an Account?</h3>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={toLoginRedirect}
+        >
+          Click Here to Sign In!
+        </button>
+      </div>
+    </div>
+  );
 }
